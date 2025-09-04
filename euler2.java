b1a41ed73b4f377231b3556772527cba5372f50c
vedanshgupta12 <vedanshgupta503@gmail.com>
2025-09-04 09:53:49 +0530

    Create euler2.java

---
+++ euler2.java
@@ -0,0 +1,17 @@
+class second
+{
+  public static void main(String[] args)
+   {
+    int sum=0;
+    for(int a=1;int b=2:b<=4000000;)
+    {
+     int fibo=a+b;
+    if(a%2==0)
+      sum+=a; 
+      a=b;
+      b=fibo;
+      fibo=a;
+System.out.println("sum="+sum);
+   }
+  }
+}
