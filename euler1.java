2c3bcc85d4d111efaab9eb164c7848242df9ef6e
vedanshgupta12 <vedanshgupta503@gmail.com>
2025-09-04 09:38:13 +0530

    Create euler1.java

---
+++ euler1.java
@@ -0,0 +1,14 @@
+class frist 
+     {
+       public static void main(String[] args) 
+     {
+      sum =233168
+      int sum=0;
+      for(int i=0;i<1000;i++)
+         { if(i%3==0||i%5==0)
+          sum+=1;
+         }
+     System.out.println("sum ="+sum);
+
+     }
+     }
